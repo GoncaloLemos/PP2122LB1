f9b0162bbc821d7d24353afbd51d526a4a65932a
Pedro S <73402295+PedroFi@users.noreply.github.com>
2022-03-21 15:45:37 +0000

    Updated UsaPessoa.java

--- src/EX1/UsaPessoa.java
+++ src/EX1/UsaPessoa.java
@@ -1,26 +1,22 @@
-package exercicio1;
+package EX1;
+
+import java.util.Scanner;
 
 public class UsaPessoa {
-    // construtor default para objetos da classe
-    // UsaPessoa este construtor existe mesmo se não
-    // for explicitamente declarado
     public UsaPessoa() {
     }
 
     public static void main(String[] args) {
         //Declara objeto pessoa1
-        Pessoa pessoa1;
- /*Cria o objeto pessoa1 que é instância da classe 
- Pessoa */
-        pessoa1 = new Pessoa();
-        //Define os valores para os atributos de pessoa1
+        Pessoa pessoa1 = new Pessoa();
         pessoa1.setNome("Camila");
         pessoa1.setGenero('F');
         pessoa1.setIdade(12);
-        System.out.println(
-                "O objeto pessoa1 chama-se " + pessoa1.getNome() +
-                        ", é do sexo " + pessoa1.getGenero() +
-                        ", tem " + pessoa1.getIdade() + " anos e diz: ");
+        System.out.println("O objeto pessoa1 chama-se " + pessoa1.getNome() + ", é do sexo " + pessoa1.getGenero() + ", tem " + pessoa1.getIdade() + " anos e diz: ");
         pessoa1.falar();
+        Pessoa P2 = new Pessoa();
+        Scanner S = new Scanner(System.in);
+        System.out.println("Nome: ");
+        Scanner.NextLine()
     }
 }
