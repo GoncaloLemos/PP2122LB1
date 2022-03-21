aba12cf5753333b790fc833f3007307dc0334f5a
Gonçalo Lemos <77722962+GoncaloLemos@users.noreply.github.com>
2022-03-21 15:28:34 +0000

    Reformatted Carro.java & Condutor.java
    Signed-off-by: Gonçalo Lemos <77722962+GoncaloLemos@users.noreply.github.com>

--- src/Carro.java
+++ src/Carro.java
@@ -1,52 +1,64 @@
 //Carro.java:
-public	class	Carro	{
-	private	String	matricula;
-	private	int	velocidadeAtual;
-	private	final	int	velocidadeMaxima	=	200;
-	private	Condutor	condutor;
-	private	boolean	ligado; 
+public class Carro {
+    private String matricula;
+    private int velocidadeAtual;
+    private final int velocidadeMaxima = 200;
+    private Condutor condutor;
+    private boolean ligado;
 
-	public	Carro(){
-								this.ligado	=	false;		
+    public Carro() {
+        this.ligado = false;
+    }
+
+    public void setMatricula(String nome) {
+        this.matricula = nome;
+    }
+
+    public void setCondutor(Condutor condutor) {
+        this.condutor = condutor;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    public int getVelocidadeAtual() {
+        return this.velocidadeAtual;
+    }
+
+    public int getVelocidadeMaxima() {
+        return this.velocidadeMaxima;
+    }
+
+    public Condutor getCondutor() {
+        return this.condutor;
+    }
+
+    void acelerar() {
+        if (this.ligado) {
+            this.velocidadeAtual += 10 + this.condutor.getDestreza() * 0.1;
+            if (this.velocidadeAtual > this.velocidadeMaxima)
+                this.velocidadeAtual = this.velocidadeMaxima;
+        } else
+            System.out.println("O	Carro	não	pode	acelerar	pois	está	desligado!");
+    }
+
+    void travar(int intensidadeTravagem) {
+        if (this.ligado) {
+            if (intensidadeTravagem == this.velocidadeMaxima)
+                intensidadeTravagem = this.velocidadeMaxima;
+            else if (intensidadeTravagem < 0)
+                intensidadeTravagem = 0;
+            this.velocidadeAtual -= intensidadeTravagem;
+            if (this.velocidadeAtual > this.velocidadeMaxima)
+                this.velocidadeAtual = this.velocidadeMaxima;
+            else if (this.velocidadeAtual < 0)
+                this.velocidadeAtual = 0;
+        } else
+            System.out.println("O	Carro	não	pode	travar	pois	está	desligado!");
+    }
 }
-					public	void	setMatricula(String	nome){
-								this.matricula	=	nome;		
-}
-				public	void	setCondutor(Condutor	condutor){
-								this.condutor	=	condutor;	
-}
-				public	String	getMatricula(){
-								return	this.matricula;		}
-				 public	int	getVelocidadeAtual(){
-								return	this.velocidadeAtual; }
-				 public	int	getVelocidadeMaxima(){
-								return	this.velocidadeMaxima;
-}
-				 public	Condutor	getCondutor(){
-								return	this.condutor;
-}
-void	acelerar()	{
-								if	(this.ligado)	{
-												this.velocidadeAtual	+=	10	+	this.condutor.getDestreza()*0.1;
-												if	(this.velocidadeAtual	>	this.velocidadeMaxima)
-																this.velocidadeAtual	=	this.velocidadeMaxima;	
-								}	else	
-											System.out.println("O	Carro	não	pode	acelerar	pois	está	desligado!");
-				}
-			void	travar(int	intensidadeTravagem)	{
-								if	(this.ligado)	{
-												if	(intensidadeTravagem	==	this.velocidadeMaxima)
-																intensidadeTravagem	=	this.velocidadeMaxima;
-									else	if	(intensidadeTravagem	<	0)
-																intensidadeTravagem	=	0;
-									this.velocidadeAtual	-=	intensidadeTravagem;
-												if	(this.velocidadeAtual	>	this.velocidadeMaxima)
-															this.velocidadeAtual	=	this.velocidadeMaxima;
-												else	if	(this.velocidadeAtual	<	0)
-															this.velocidadeAtual	=	0;	
-								}	else
-												System.out.println("O	Carro	não	pode	travar	pois	está	desligado!");		}		
-}
-public	void	buzinar()	{
-		System.out.println(“Buzina	do	carro”);
-}	
+
+    public void buzinar() {
+        System.out.println(“Buzina do carro”);
+    }
--- src/Condutor.java
+++ src/Condutor.java
@@ -1,23 +1,34 @@
 //Condutor.java:
-public	class	Condutor	{
-				//Atributos
-				private	String	nome;
-				private	int	idade;
-				private	int	destreza;
-	public	Condutor(){}
-				public	void	setNome(String	nome){
-								this.nome	=	nome;}
-				
-				public	void	setIdade(int	idade){
-								this.idade	=	idade;}
-				
-				public	void	setDestreza(int	destreza){
-								this.destreza	=	destreza;				}
-				
-				public	String	getNome(){
-								return	this.nome;}
-				public	int	getIdade(){
-								return	this.idade;}
-				public	int	getDestreza(){
-								return	this.destreza;}
+public class Condutor {
+    //Atributos
+    private String nome;
+    private int idade;
+    private int destreza;
+
+    public Condutor() {
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public int getDestreza() {
+        return this.destreza;
+    }
 }	
